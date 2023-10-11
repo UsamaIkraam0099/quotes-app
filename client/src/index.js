08bcb6542e64a492e6a5cec56d18ee8c4398a948
UsamaIkraam0099 <usamaikraam0099@gmail.com>
2023-10-11 16:59:00 +0400

    working on graph ql client side

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,19 @@ import ReactDOM from "react-dom/client";
 // Others
 import Routes from "./routes";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Routes />
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,5 +1,5 @@
 import "./style.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Others
 import { NavBar } from "../../comman";
@@ -20,6 +20,7 @@ const data = [
 ];
 
 let initialState = {
+  quotes: [],
   show: false,
   form: {
     quote: {
@@ -32,10 +33,40 @@ let initialState = {
 };
 
 const Dashboard = () => {
-  const [{ show, form }, setState] = useState(initialState);
+  const [{ form, show, quotes }, setState] = useState(initialState);
 
   const updateState = (state) =>
     setState((prevState) => ({ ...prevState, ...state }));
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  const getQuotes = async () => {
+    const response = await fetch("http://localhost:4000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTE0MTc5MWFjMzFhYTAwODE0YzFhOWYiLCJpYXQiOjE2OTcwMjE0MzB9.bNr1H-Z-eO4HsZNIqwAlMSVsyfE5J2eNQU9srHw7u1k",
+      },
+      body: JSON.stringify({
+        query: `
+    query getAllQuotes {
+      quotes{
+        name
+        by
+      }
+    }
+    `,
+      }),
+    });
+
+    const res = await response.json();
+
+    if (res.errors?.length > 0) {
+    } else updateState({ quotes: res.data.quotes });
+  };
 
   const handleInputChange = (name, value) => {
     let formCopy = { ...form };
@@ -70,31 +101,32 @@ const Dashboard = () => {
     <section>
       <NavBar handlePress={handlePress} />
 
-      <div className="container flex-column flex-column align-items-center justify-content-center">
-        <div className="d-flex flex-column align-items-center justify-content-center">
-          <img
-            src="https://robohash.org/set_set3/bgset_bg1/3.14159?size=200x200"
-            className="img-thumbnail"
-            alt="profile"
-          />
+      <div className="d-flex flex-column align-items-center justify-content-center mt-5">
+        <img
+          src="https://robohash.org/set_set3/bgset_bg1/3.14159?size=200x200"
+          className="img-thumbnail"
+          alt="profile"
+        />
 
-          <h5 className="mt-2">Usama Ikraam</h5>
-          <p className="mt-1">usama.ikraam@al-ghurair.com</p>
-        </div>
+        <h5 className="mt-2">Usama Ikraam</h5>
+        <p className="mt-1">usama.ikraam@al-ghurair.com</p>
+      </div>
 
-        <div className="d-flex flex-column align-items-center justify-content-center">
-          <ul>
-            {data.map((item, index) => {
-              return (
-                <div className="card mt-3 quotes-card" key={`-${index}`}>
-                  <div className="card-body">
-                    <h6>{item.name}</h6>
-                  </div>
+      <div className="d-flex flex-column align-items-center justify-content-center">
+        <ul>
+          {quotes.map((item, index) => {
+            return (
+              <div
+                className="card mt-3 quotes-card"
+                key={`${index}-${item.by}`}
+              >
+                <div className="card-body">
+                  <h6>{item.name}</h6>
                 </div>
-              );
-            })}
-          </ul>
-        </div>
+              </div>
+            );
+          })}
+        </ul>
       </div>
 
       <Modal
